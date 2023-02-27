cd0ec2a5c37e4e11117e7425461aa5c86d078d63
AveMaria23 <demyn300@gmail.com>
2023-02-27 20:48:22 +0300

    ui logic

--- app/src/main/java/com/example/vktaskui/MainActivity.kt
+++ app/src/main/java/com/example/vktaskui/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.vktaskui
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.core.content.ContextCompat
+import com.example.vktaskui.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val buttonCamera = binding.camera
+        var cameraFlag = true
+        buttonCamera.setOnClickListener {
+            if (cameraFlag){
+                buttonCamera.setImageResource(R.drawable.videocam)
+                cameraFlag = false
+            } else {
+                buttonCamera.setImageResource(R.drawable.videocam_off)
+                cameraFlag = true
+            }
+        }
     }
 }
