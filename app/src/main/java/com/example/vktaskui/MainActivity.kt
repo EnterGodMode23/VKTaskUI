21e1d30d39b20a9b3733d7f78164ffe4f82e05b0
AveMaria23 <demyn300@gmail.com>
2023-02-28 19:52:28 +0300

    logic

--- app/src/main/java/com/example/vktaskui/MainActivity.kt
+++ app/src/main/java/com/example/vktaskui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.vktaskui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
@@ -13,10 +14,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val buttonMessage = binding.message
+
+        val card1 = binding.firstCard
+        val card2 = binding.secondCard
+        val buttonGroup = binding.group
+
         val buttonCamera = binding.camera
         val buttonMic = binding.bottomMic
         val buttonHand = binding.hand
         val buttonEndCall = binding.endCall
+
+        buttonMessage.setOnClickListener{
+            val it = Intent(Intent.ACTION_VIEW)
+            it.setType("vnd.android-dir/mms-sms")
+            startActivity(it)
+        }
+
         buttonCamera.setOnClickListener{
             BottomActions.cameraSwitch(buttonCamera)
         }
